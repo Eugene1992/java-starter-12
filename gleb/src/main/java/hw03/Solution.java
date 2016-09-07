ff906bc9279663ce29b1ec1411adf81cc8f4ed4c
Eugene1992 <deyneko55@gmail.com>
2016-09-07 09:40:27 +0300

    Test commit.

---
+++ gleb/src/main/java/hw03/Solution.java
@@ -0,0 +1,10 @@
+package hw03;
+
+/**
+ * Created by Student on 9/7/2016.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
