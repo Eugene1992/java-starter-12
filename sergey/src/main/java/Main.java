bb52c3dedab3043320eb03407b171eeb8503946b
nixig <nixigg@gmail.com>
2016-09-07 01:27:21 -0700

    hw01

--- sergey/src/main/java/Main.java
+++ sergey/src/main/java/Main.java
@@ -2,4 +2,30 @@
  * Created by evgeniy on 06/09/16.
  */
 public class Main {
+
+    public static void main(String[] args) {
+        byte a;
+        short b;
+        int c;
+        long d;
+        a = 5+5;
+        b = 25-15;
+        c = 30 / 10;
+        d = 5 * 6;
+        a = 10 + 10;
+        a = 10 + 10;
+        a = 1;
+        c = (10 / 10) * (1 * 1);
+        c = c+a;
+        d = d+a;
+        c = c+c;
+        d = d+d;
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+    }
 }
