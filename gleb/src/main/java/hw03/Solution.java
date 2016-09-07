d39f4a51c023e6d3e28b998b2f67ca81173d57f5
Eugene1992 <deyneko55@gmail.com>
2016-09-07 09:45:49 +0300

    Test commit.

--- gleb/src/main/java/hw03/Solution.java
+++ gleb/src/main/java/hw03/Solution.java
@@ -6,5 +6,6 @@ package hw03;
 public class Solution {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
