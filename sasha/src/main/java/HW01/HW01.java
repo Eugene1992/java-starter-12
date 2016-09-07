9864f51b85c6e6636cef73bd050e125096700f92
Sasha <faskit97@gmail.com>
2016-09-07 13:48:58 +0300

    Home Work01

---
+++ sasha/src/main/java/HW01/HW01.java
@@ -0,0 +1,58 @@
+package HW01;
+
+/**
+ * Created by Sasha on 06.09.2016.
+ */
+    public class HW01 {
+    //1. Создать четыре целочисленные переменные типов byte, short, int, long.
+     public static void main(String[] args) {
+        /**byte a = 127;
+        short b = 64;
+        int c = 86;
+        long d = 112;
+        //2.Инициализировать переменную типа byte результатом суммы двух любых чисел
+        byte z = (byte)(50+41);
+        System.out.println(z);
+        //3. Инициализировать переменную типа short  результатом вычитания двух любых чисел
+        short z = (short)(21-19);
+        System.out.println(z);
+        //4. Инициализировать переменную типа int результатом деления двух любых чисел
+        int x = (int)(30/10);
+        System.out.println(x);
+        //5. Инициализировать переменную типа long результатом умножения двух любых чисел
+        long x =(long)(2*2);
+        System.out.println(x);
+        //6. Передать переменной типа byte результат суммы двух переменных типа byte
+        byte x = 15;
+        byte y = 15;
+        byte z = (byte)(x+y);
+        System.out.println(z);
+        //7. Передать переменной типа byte результат суммы двух переменных типа short, деленных на третью переменную типа byte
+        byte b = 24;
+        byte c = 5;
+        short z = 25;
+        short y = 43;
+        byte d = (byte)(z+y/c);
+        System.out.println(d);
+        //8. Переменной типа int передать результат деления двух short, умноженных на результат деления двух byte
+        byte a = 6;
+        byte b = 3;
+        short z = 10;
+        short y = 2;
+        int x = (int)(z/y)*(a/b);
+        System.out.println(x);
+        //9. Поменять значения двух целочисленных переменных используя третью
+        byte a = 15;
+        byte b = 15;
+        byte c = 21;
+        System.out.println(a=c);
+        System.out.println(b=c);
+        //10. Поменять значения двух переменных без использования третей
+        byte a = 25;
+        byte b = 35;
+        System.out.println(a+b);
+        System.out.println(b-a);*/
+
+
+    }
+}
