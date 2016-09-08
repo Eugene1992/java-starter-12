52d48a39eb03229edcf74cad931c844bbbe97875
nixig <nixigg@gmail.com>
2016-09-08 00:32:21 -0700

    hw03+question

---
+++ sergey/src/main/java/hw09072016/Homework4.java
@@ -0,0 +1,21 @@
+package hw09072016;
+
+import java.util.Scanner;
+
+/**
+ * Created by nixig on 9/7/2016.
+ */
+public class Homework4 {
+
+    public static void main(String[] args) {
+        for (int counter = 1; counter < 11; counter++) {
+            System.out.println(counter);
+
+        }
+    }
+
+
+
+    }
+
+
