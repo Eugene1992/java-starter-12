a0cd9649865445423b0f19b6c29d64f3ff1731cc
Gleb73 <Tarasevic.hleb@gmail.com>
2016-09-12 09:46:54 +0300

    Test

---
+++ gleb/src/main/java/Hw/Test.java
@@ -0,0 +1,20 @@
+package Hw;
+
+/**
+ * Created by Gleb on 09.09.2016.
+ */
+public class Test {
+
+    public static void main (String arg []){
+
+        int a = 1000;
+
+        for (int i = 1; i < 20; i++ )
+        {
+            a += 3;
+            System.out.print(a + " ");
+        }
+
+    }
+
+}
---
+++ gleb/src/main/java/Hw/Test4.java
@@ -0,0 +1,20 @@
+package Hw;
+
+/**
+ * Created by Gleb on 09.09.2016.
+ */
+public class Test4 {
+
+    public static void main (String arg [])
+    {
+        int v = 1;
+
+        for (int i = 0; i < 20; i++)
+
+        {
+            v *= 2;
+            System.out.print(v + " ");
+        }
+    }
+
+}
