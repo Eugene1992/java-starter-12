e91babb4dd216bf907b2c24d1f8e5a1dc1d9b352
nixig <nixigg@gmail.com>
2016-09-11 20:35:02 -0700

    hw03+question

---
+++ sergey/src/main/java/hw09112016/Homew4.java
@@ -0,0 +1,14 @@
+package hw09112016;
+
+/**
+ * Created by nixig on 9/11/2016.
+ */
+public class Homew4 {
+    public static void main(String[] args) {
+        for (int count = 1; count < 500; count *=2){
+            System.out.println(count);
+
+
+        }
+    }
+}
