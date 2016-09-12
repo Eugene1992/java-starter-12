521400ffb185bbf51c5d1f5b87d25d8eb08f389a
nixig <nixigg@gmail.com>
2016-09-11 20:20:18 -0700

    hw03+question

---
+++ sergey/src/main/java/hw09112016/Homew2.java
@@ -0,0 +1,17 @@
+package hw09112016;
+
+/**
+ * Created by nixig on 9/11/2016.
+ */
+public class Homew2 {
+    public static void main(String[] args) {
+        for (int count = 1; count < 57; count+=2){
+            System.out.println(count);
+
+
+        }
+    }
+
+}
+
+
