dabd90185b57e1e90ac1b660691a684834ae5357
nixig <nixigg@gmail.com>
2016-09-12 01:27:59 -0700

    hw03+question

---
+++ sergey/src/main/java/cw09122016/Linl.java
@@ -0,0 +1,21 @@
+package cw09122016;
+
+/**
+ * Created by nixig on 9/12/2016.
+ */
+public class Linl {
+    public static void main(String[] args) {
+        for (int a = 0; a < 10; a++ ) {
+            for (int j = 0; j < a; j++){
+                System.out.print("# ");
+
+
+
+
+            }
+
+          System.out.println();
+        }
+    }
+
+}
