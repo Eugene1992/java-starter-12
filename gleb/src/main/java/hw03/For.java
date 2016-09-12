123d5f0e2ff99b1fe0eafbeda0592a1d723ea6c2
Gleb73 <Tarasevic.hleb@gmail.com>
2016-09-12 09:46:44 +0300

    Test

---
+++ gleb/src/main/java/hw03/For.java
@@ -0,0 +1,21 @@
+package hw03;
+
+/**
+ * Created by Gleb on 08.09.2016.
+ */
+public class For {
+
+   /* public static void main (String arg[]) {
+         String s = "";
+
+        for (int i = 100; i != 0 ; i /= 2)
+
+        {
+            s = s + i%2;
+
+        }System.out.print(s);
+
+
+
+    }*/
+}
