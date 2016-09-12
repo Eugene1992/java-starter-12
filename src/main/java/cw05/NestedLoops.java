3748a75024c58796021e3c5311720ed2e9d1c8ad
mankunianeth <deyneko55@gmail.com>
2016-09-12 11:30:21 +0300

    Add hw04.

--- src/main/java/cw05/NestedLoops.java
+++ src/main/java/cw05/NestedLoops.java
@@ -4,11 +4,10 @@ package cw05;
  * Created by evgeniy on 12/09/16.
  */
 public class NestedLoops {
-    public static void main(String[] args) throws InterruptedException {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                System.out.print("# ");
-                Thread.sleep(1000);
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
